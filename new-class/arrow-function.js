a5b664389052a5600f1d1784ad95087d6edf85fb
Harsh <harshgupt100@gmail.com>
2024-11-12 14:30:28 +0530

    new class

---
+++ new-class/arrow-function.js
@@ -0,0 +1,24 @@
+let b = ()=>{return "js"}
+console.log(b())
+
+//syntax 1.
+let a = (a,b)=>{return a + b}
+console.log(a(2,3))
+
+//syntax 2.
+//()=>()
+
+//3.
+//()=>
+
+//4.
+//=>
+
+
+// wap to find area of circle using arrow function.
+
+let area = (r)=>{return 3.14*r*r}
+console.log(area(2
+    
+))
+
