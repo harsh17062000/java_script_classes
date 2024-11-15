88f3e4728515c0966b2a19073d9530f1e0d1a57b
Harsh <harshgupt100@gmail.com>
2024-11-15 14:32:00 +0530

    added login pages

--- new-class/obj.js
+++ new-class/obj.js
@@ -1,24 +1,46 @@
-let obj = {
-    name : "harsh",
-    age : 24,
-    contact : 4567
+// let obj = {
+//     name : "harsh",
+//     age : 24,
+//     contact : 4567
+// }
+
+// console.log(obj.age)
+// for (k in obj) 
+//     {
+//         console.log(k)  //for displaying key word.
+//         console.log(obj[k]) //for displaying key value.
+//     } 
+
+// // . dot is known as "access member operater"
+// // ; semicolon is known as "terminator"
+
+// let obj1 =
+//  {
+//     name : document.querySelector("#name").value,
+//     age : document.querySelector("#age").value,
+//     contact : document.querySelector("#contact").value
+// }
+
+// localStorage.setItem("data",JSON.stringify(obj))
+
+
+function save()
+{
+    let data = {
+        name : document.querySelector('#name').value,
+        age : document.querySelector('#age').value,
+        contact : document.querySelector('#contact').value
+    }
+    localStorage.setItem("mydata",JSON.stringify(data))
 }
 
-console.log(obj.age)
-for (k in obj) 
-    {
-        console.log(k)  //for displaying key word.
-        console.log(obj[k]) //for displaying key value.
-    } 
+let m = JSON.parse(localStorage.getItem("mydata"))
+console.log(m);
 
-// . dot is known as "access member operater"
-// ; semicolon is known as "terminator"
+let myname = document.querySelector('#myname')
+let myage = document.querySelector('#myage')
+let mycontact = document.querySelector('#mycontact')
 
-let obj1 =
- {
-    name : document.querySelector("#name").value,
-    age : document.querySelector("#age").value,
-    contact : document.querySelector("#contact").value
-}
-
-localStorage.setItem("data",JSON.stringify(obj))
+myname.innerHTML = m.name;
+myage.innerHTML = m.age;
+mycontact.innerHTML = m.contact;
