8890ceae8edf697b6a014a96312e7de9e13823c8
Harsh <harshgupt100@gmail.com>
2024-11-16 14:29:39 +0530

    module

---
+++ new-class/module/index.js
@@ -0,0 +1,7 @@
+import harsh from './add.js'
+console.log(harsh(3,4));
+import {a,b} from './index2.js'
+
+a()
+b()
+//you can change the function name in main file if the another file only having 1 function.
---
+++ new-class/module/index2.js
@@ -0,0 +1,9 @@
+export function a() {
+console.log("a function")
+}
+
+export function b() {
+    console.log("b function")
+}
+
+//export{a,b}
